dc920de7da20906798e1e4a81d5fe5904e03a1d8
okanaras <oknaras@icloud.com>
2024-06-24 17:27:20 +0300

    Redux Toolkit | axios istegi | slice  kullanimlari

---
+++ src/redux/slices/productSlice.jsx
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios';
+
+const initialState = {
+    products: [],
+    selectedProduct: {},
+    loading: false
+}
+
+const BASE_URL = "https://fakestoreapi.com";
+console.log('test', `${BASE_URL}/products`);
+
+export const getAllProducts = createAsyncThunk("getAllProducts", async () => {
+    const response = await axios.get(`${BASE_URL}/products`);
+    return response.data;
+});
+
+export const productSlice = createSlice({
+    name: "product",
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getAllProducts.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(getAllProducts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.products = action.payload;
+        })
+    }
+});
+
+export const { } = productSlice.actions;
+
+export default productSlice.reducer;
+
