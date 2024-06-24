97cf17636ea56658ee580efb0be12c9c143ff1b5
okanaras <oknaras@icloud.com>
2024-06-24 16:36:59 +0300

    Arayuz tasarimi yapildi - 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import '../css/Header.css';
+import { CiSearch } from "react-icons/ci";
+import { CiShoppingBasket } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
+import { AiFillMoon } from "react-icons/ai";
+
+function Header() {
+    const [theme, setTheme] = useState(false);
+
+    useEffect(() => {
+        const root = document.querySelector("#root");
+        if (theme) {
+            root.style.backgroundColor = "#000";
+            root.style.color = "#fff";
+        } else {
+            root.style.backgroundColor = "#fff";
+            root.style.color = "#000";
+        }
+    }, [theme]);
+
+    const changeTheme = () => {
+        setTheme(!theme);
+    };
+
+    return (
+        <div className='header-wrapper'>
+            <div className='flex-row'>
+                <img className='logo' src={theme ? './src/images/logo-light-mode.png' : './src/images/logo-dark-mode.png'} />
+                <p className='logo-text'>OKAN A.S</p>
+            </div>
+            <div className='search-div flex-row'>
+                <input type='text' className='search-input' placeholder='Ara' />
+                {/* <CiSearch className='search-icon' /> */}
+                <div>
+                    {theme ? <CiLight className='icon' onClick={changeTheme} /> : <AiFillMoon className='icon' onClick={changeTheme} />}
+
+                    <CiShoppingBasket className='icon' />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
